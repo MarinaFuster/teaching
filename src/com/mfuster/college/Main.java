39e56f3c42a009d49e9ff664d0d2849f41d6b2bf
marina <mfuster@itba.edu.ar>
2021-11-23 11:07:23 -0300

    :sparkles: INIT: teaching project

---
+++ src/com/mfuster/college/Main.java
@@ -0,0 +1,15 @@
+package com.mfuster.college;
+
+// HOMEWORK:
+//   - Print the college name for all students.
+//   - Create two students (you can choose their names)
+//   - Set gpa of 3.4 for first student and 2.4 for second student
+//   - Print both students' names and lastnames
+//   - Set gpa of 3.8 for first student
+//   IMPORTANT
+//     - You must be able to complete this in order
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/com/mfuster/college/Student.java
@@ -0,0 +1,12 @@
+package com.mfuster.college;
+
+// HOMEWORK:
+// Complete the class Student, where each student has:
+//   - the name of college (it is the same for ALL students)
+//   - the name of the student
+//   - the lastname of the student
+//   - the gpa of the student
+//   Important things to consider:
+//     - the name of the college, name of the student and lastname of the student cannot change.
+public class Student {
+}
