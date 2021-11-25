a743be9608876d956f2dd67991f6491d2da6eac5
napodevesa <napodevesa@gmail.com>
2021-11-24 22:33:33 -0300

    stating2

--- src/com/mfuster/Main.java
+++ src/com/mfuster/Main.java
@@ -4,10 +4,5 @@ import com.mfuster.college.Student;
 
 public class Main {
 
-    public static void main(String[] args) {
-    
-    	Student numberOne = new Student ("MarinaCollege", "Napo","Devesa",8); 
-    	Student numberTwo = new Student ("BaCollege", "Laura","Dal",4) ;
-    	
-    }
+
 }
--- src/com/mfuster/college/Main.java
+++ src/com/mfuster/college/Main.java
@@ -11,6 +11,7 @@ package com.mfuster.college;
 public class Main {
     public static void main(String[] args) {
     	
+    	
     	//Create two students (you can choose their names)
     	Student numberOne = new Student ("MarinaCollege", "Napo","Devesa",8); 
     	Student numberTwo = new Student ("BaCollege", "Laura","Dal",4) ;
@@ -18,23 +19,36 @@ public class Main {
     	//Print the college name for all students.
     	String numberOneCollege= numberOne.getNameCollege();
     	String numberTwoCollege= numberTwo.getNameCollege();
+    
     	System.out.println(numberOneCollege + " " +  numberTwoCollege);
     	
     	//Set gpa of 3.4 for first student and 2.4 for second student
     	numberOne.setGpa(3.4);
     	numberTwo.setGpa(2.4);
     	
-    	//Print both students' names and lastnames
+
     	String numberOneName= numberOne.getNameStudent();
     	String numberOneSurname= numberOne.getSurnameStudent();
+    	
     	String numberTwoName= numberTwo.getNameStudent();
     	String numberTwoSurname= numberTwo.getSurnameStudent();
-
-    	System.out.println("numberOne: ");
-    	System.out.println(numberOneName + " " +  numberOneSurname);
     	
-    	System.out.println("numberTwo: ");
-    	System.out.println(numberTwoName + " " + numberTwoSurname);
+    	
+    	printName(numberTwoName, numberTwoSurname);
+    	printName(numberOneName, numberOneSurname);
+
 
     }
+
+	private static void printName(String nameStudent, String surnameStudent) {
+		System.out.println(nameStudent + " " + surnameStudent);		
+	}
+	
+	private static void printNameCollege(String nameCollege) {
+		System.out.println(nameCollege);		
+	}
+	
+	
+	
+	
 }
