e6d42d933770a09dcc9d39426e6b7d4b1d2717a8
napodevesa <napodevesa@gmail.com>
2021-12-09 18:00:36 -0300

    clase 9 de diciembre, interfases y comparator

--- src/com/mfuster/library/Book.java
+++ src/com/mfuster/library/Book.java
@@ -1,4 +1,33 @@
 package com.mfuster.library;
 
-public class Book {
+public class Book implements Comparable <Book> {
+	
+	private String title;
+	private String author;
+	private int year;
+	
+	public Book(String title,String author,int year){
+		this.title=title;
+		this.author=author;
+		this.year=year;
+		
+		}
+	
+	public String getTitle() {
+		return title;
+	}
+	
+	public String getAuthor() {
+		return author;
+	}
+	
+	public int getYear() {
+		return year;
+	}
+	
+	public int compareTo(Book book) {	
+		//this.title.compareTo(book.title);	
+		return this.title.compareTo(book.title);
+	}
+
 }
--- src/com/mfuster/library/Main.java
+++ src/com/mfuster/library/Main.java
@@ -8,6 +8,7 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
+    	
         // *************** PART ONE ***************
         // We want a program where we can create a sorted list of books
         // A book has a title, author and publication date.
@@ -15,14 +16,43 @@ public class Main {
         // I want to sort those books according to their title.
         // Assumption: two books can't have the same title.
 
-
         // *************** PART TWO ***************
         // What if I want three lists? One sorted by title first, other sorted by author first
         // and other sorted by date first.
 
-
-
         // *************** PART THREE ***************
-        // I want to create a SET of books, taken from the list.
+    	// I want to create a SET of books, taken from the list.
+    	
+    
+    	
+    	Book harryPotter = new Book("Harry Potter", "J.K.Rowling", 1997);
+        Book frankenstein = new Book("Frankenstein", "Mary Shelley", 1818);
+        Book hungerGames = new Book("Hunger Games", "Suzanne Collins", 2008);
+        Book prideAndPrejudice = new Book("Pride and Prejudice", "Jane Austen", 1813);
+        Book wizardOfOz = new Book("Wizard of Oz", "Frank Baum", 1900);
+    	
+        //interface list con ArrayList (clase)
+        List <Book> listBook = new ArrayList <Book> ();
+        
+        listBook.add(harryPotter);
+        listBook.add(frankenstein);
+        listBook.add(hungerGames);
+        listBook.add(prideAndPrejudice);
+        listBook.add(wizardOfOz);
+           
+        //no se puede llamar a métodos que no esten en List:
+        //listBook.sort(c);
+        
+        Collections.sort(listBook);
+        printBookstore( listBook , "tite" );
+
+    }
+    
+    public static void printBookstore(List<Book> bookstore, String sortingElement) {
+        System.out.printf("Book sorted by %s%n", sortingElement);
+        for(Book b : bookstore) {
+            System.out.printf("%s (%s) - %d%n", b.getTitle(), b.getAuthor(), b.getYear());
+        }
+        System.out.println("\n");
     }
 }
