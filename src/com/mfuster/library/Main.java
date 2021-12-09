510efbad2e98e13c29d4c352a2112e683faf4ad1
marina <mfuster@itba.edu.ar>
2021-12-09 15:28:55 -0300

    :zap: ADD: exercise assumption

--- src/com/mfuster/library/Main.java
+++ src/com/mfuster/library/Main.java
@@ -1,5 +1,10 @@
 package com.mfuster.library;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -7,12 +12,14 @@ public class Main {
         // We want a program where we can create a sorted list of books
         // A book has a title, author and publication date.
         // First, I want to be able to create a LIST of books, like if it is a bookstore.
-        // I want to sort those books for: title -> author -> publication date.
+        // I want to sort those books according to their title.
+        // Assumption: two books can't have the same title.
 
 
         // *************** PART TWO ***************
         // What if I want three lists? One sorted by title first, other sorted by author first
         // and other sorted by date first.
+
 
 
         // *************** PART THREE ***************
